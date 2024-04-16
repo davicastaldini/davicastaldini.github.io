1df5135b5c868dddda00bbe741a346771107c22b
davicastaldini <davc1524@gmail.com>
2024-04-16 11:29:15 -0300

    init

---
+++ index_animação.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    console.log("Página totalmente carregada!");
+    setTimeout(function() {
+        document.querySelector('.logomidas').style.bottom = '0';
+    }, 500); // Tempo de espera antes da animação começar (em milissegundos)
+});
+
+
